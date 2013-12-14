64dcf6b607020efc9be532c630431d6d906569e1
JackonYang <jiekunyang@gmail.com>
2013-12-14 10:18:58 +0800

    debug log without config

---
+++ wise_log.py
@@ -0,0 +1,35 @@
+"""log in 4 files"""
+
+import logging
+import os
+
+log_lvl = logging.DEBUG
+
+
+def get_wise_logger(formatter, path, name=''):
+    log = logging.getLogger(name)
+    log.setLevel(log_lvl)
+    lvls = ['debug', 'info', 'warn', 'error']
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for lvl in lvls:
+        logfile = os.path.join(path, '{}.log'.format(lvl.lower()))
+        hdlr = logging.FileHandler(logfile)
+        hdlr.setLevel(getattr(logging, lvl.upper()))
+        hdlr.setFormatter(formatter)
+        log.addHandler(hdlr)
+    return log
+
+
+def debug_log(rel_path='../log/debug'):
+    path = os.path.join(os.path.dirname(__file__), rel_path)
+    formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(message)s|%(filename)s-%(lineno)s')
+    return get_wise_logger(formatter, path, 'debug')
+
+
+if __name__ == '__main__':
+    log = debug_log(rel_path='log/debug')
+    log.error('test log')
+    log.info('info log')
